4e4110f56d4b63655a7458b4e2b1a0404cb008e4
wrzhxy <wrzhxy@qq.com>
2019-04-27 11:37:57 +0800

    。

--- first-docker-compose/mysql/init/init.sql
+++ first-docker-compose/mysql/init/init.sql
@@ -1,14 +1,14 @@
 -- 添加新用户
--- CREATE USER 'traveler'@'%' IDENTIFIED BY 'ALKdks348,.';
+-- CREATE USER 'bloger'@'%' IDENTIFIED BY 'ALKdks348,.';
 -- create database blog;
 use mysql;
-grant all privileges on travel.* to traveler@'%';
+grant all privileges on blog.* to bloger@'%';
 -- alter user 'traveler'@'%' identified with mysql_native_password by 'ALKdks348,.';
 flush privileges;
 
-use travel;
-CREATE TABLE logs (
-	id varchar(40) primary key,			            -- 主键
-	name VARCHAR(20) NOT NULL DEFAULT '',			-- 名称
-	location varchar(100) NOT NULL DEFAULT ''	    -- 描述
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- use blog;
+-- CREATE TABLE logs (
+-- 	id varchar(40) primary key,			            -- 主键
+-- 	name VARCHAR(20) NOT NULL DEFAULT '',			-- 名称
+-- 	location varchar(100) NOT NULL DEFAULT ''	    -- 描述
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
